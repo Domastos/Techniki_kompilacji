5245bfc9221ccb80a7dce8e4d07f1e02528f199c
Domastos <isavchenko18@gmail.com>
2020-06-06 21:47:29 +0200

    add aditional rules to lexer

---
+++ symtable.hpp
@@ -0,0 +1,20 @@
+#ifndef TK_SYMTABLE_H
+#define TK_SYMTABLE_H
+
+#pragma once
+#include "parser.hpp"
+
+enum Relop : int
+{
+    NotEqual,
+    LesserEqual,
+    GreaterEqual,
+    Greater,
+    Equal,
+    Lesser
+};
+
+
+
+
+#endif //TK_SYMTABLE_H
