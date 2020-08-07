91d1b7a0d61e795b662fb822e3d60d4a7f5a2ac6
Domastos <isavchenko18@gmail.com>
2020-08-07 22:53:23 +0200

    changes in tests

--- compare.py
+++ compare.py
@@ -5,7 +5,8 @@ import re
 
 
 if len(sys.argv) != 3:
-    print("TEST ERROR: wrong number of arguments")
+    print("\033[1;31;40m TEST ERROR: wrong number of arguments")
+    sys.exit(-1)
 
 file = (sys.argv[1], sys.argv[2])
 lines = []
@@ -19,7 +20,8 @@ with open(file[1]) as f:
 fail = True if len(lines[0]) != len(lines[1]) else False
 
 if fail:
-    print(f"TEST FAILED: {sys.argv[1]}")
+    print(f"\033[1;31;40m TEST FAILED: {sys.argv[1]}")
+    sys.exit(-1)
 
 comment = re.compile(r";.*")
 white = re.compile(r"[ \t]+")
@@ -30,6 +32,18 @@ def fix_line(line):
     line = line.strip()
     return line
 
+lines = [fix_line(line) for line in lines[i] for i in range(len(lines))]
+
+if not fail:
+    for i,k in enumerate(lines[0]):
+        if lines[0][i] != lines[1][i]:
+            fail = True
+            print("\033[1;31;40m FAIL (%d)" % (i+1))
+            print(f"{lines[0][i]}\n{lines[1][i]}")
+            break
+
+    if not fail:
+        print("\033[1;32;40m TEST PASSED")
 
 
  
